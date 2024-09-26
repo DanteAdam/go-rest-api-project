a3ae38739dd4caf1c44737e336a0d001054c3c27
Nguyen Duy Tuan <tuannguyenduy2@gmail.com>
2024-09-27 00:06:58 +0700

    update

--- models/event.go
+++ models/event.go
@@ -4,18 +4,18 @@ import "time"
 
 type Event struct {
 	ID          int
-	Name        string
-	Description string
-	Location    string
-	DateTime    time.Time
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
 	UserID      int
 }
 
 var events = []Event{}
 
-func (e *Event) Save() {
+func (e Event) Save() {
 	//add to database later
-	events = append(events, *e)
+	events = append(events, e)
 }
 
 func GetAllEvents() []Event {
